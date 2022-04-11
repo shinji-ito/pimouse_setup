9c5796a7eca64e1b4927d07326510c65e2fc39cf
Shinji Ito <shinji-ito@ogawakiko.com>
2022-04-12 00:42:07 +0900

    Add first commit

---
+++ setup.bash
@@ -0,0 +1,11 @@
+#!/bin/bash -xve
+
+exec 2> /tmp/setup.log		#シェルスクリプトでログを記録
+
+cd /home/ubuntu/RaspberryPiMouse/src/drivers/
+/sbin/insmod rtmouse.ko		#rtmouse.koがデバイスドライバ本体
+
+sleep 1				#すぐにはデバイスファイルができないので待つ
+chmod 666 /dev/rt*
+
+echo 0 > /dev/rtmotoren0	#安全のためにモーター電源オフ
